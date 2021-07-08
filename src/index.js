de2d5ac2e7c206969a4dbbea4ef4d5cab15781ac
hzcy <609995485@qq.com>
2021-07-08 12:39:26 +0800

    U23

--- src/index.js
+++ src/index.js
@@ -48,6 +48,60 @@ app.get("/browser/:name", async (req, res) => {
   }
 });
 
+
+app.get("/test/:name", async (req, res) => {
+  const browserName = req.params["name"] || "chromium"
+  if (!["chromium", "firefox"].includes(browserName)) {
+    return res.status(500).send(`invalid browser name (${browserName})!`)
+  }
+  const url =  "https://ide.goorm.io/"
+  const waitUntil = req.query.waitUntil || "load"
+  const width = req.query.width ? parseInt(req.query.width, 10) : 1920
+  const height = req.query.height ? parseInt(req.query.height, 10) : 1080
+  console.log(`Incoming request for browser '${browserName}' and URL '${url}'`)
+  try {
+    /** @type {import('playwright-chromium').Browser} */
+    const browser = await { chromium, firefox }[browserName].launch({
+      chromiumSandbox: false
+    })
+    const page = await browser.newPage({
+      viewport: {
+        width,
+        height
+      }
+    })
+    await page.goto(url, {
+      timeout: 10 * 1000,
+      waitUntil
+    })
+    if (req.query.timeout) {
+      await page.waitForTimeout(parseInt(req.query.timeout, 10))
+    }
+    //
+    await page.click("text=Sign In");
+    await page.click("#emailInput");
+
+    await page.fill("#emailInput", "huanghe95@outlook.com");
+    await page.click("#passwordInput");
+    await page.fill("#passwordInput", "Hh18727353590$");
+    await page.click("#keep-login");
+    await page.click("text=Login");
+    await page.click('[aria-label="Skip"]');
+
+    // const data = await page.screenshot({
+    //   type: "png"
+    // })
+    const data = await page.content();
+    await browser.close()
+    //res.contentType("image/png")
+    res.contentType("text/html")
+    res.set("Content-Disposition", "inline;");
+    res.send(data)
+  } catch (err) {
+    res.status(500).send(`Something went wrong: ${err}`)
+  }
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
